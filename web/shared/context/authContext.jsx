5ee95e143a4b14c56c3dfb9d193e679d889d92eb
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-07-18 18:36:50 +0200

    fixed authContext to return values and fixed bug causing recursion

--- web/shared/context/authContext.jsx
+++ web/shared/context/authContext.jsx
@@ -4,43 +4,66 @@ import * as auth from "../api/auth.js";
 
 const AuthContext = createContext();
 
-export function AuthProvider({ children }) {
+export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
+  // Set the the user`s JWT in the storage.
   const login = async (credentials) => {
-    const result = await auth.login(credentials);
+    setLoading(true);
+    try {
+      const result = await auth.login(credentials);
 
-    localStorage.setItem("token", result.token);
+      localStorage.setItem("token", result.token);
 
-    setUser(result.user);
+      setUser(result.user);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  // Remove the user's JWT from storage
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
   };
 
+  // Load user`s data
+  const loadUser = async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const user = await auth.me();
+      setUser(user);
+    } catch {
+      localStorage.removeItem("token");
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const loadUser = async () => {
-      const token = localStorage.getItem("token");
-
-      if (!token) {
-        setLoading(false);
-        return;
-      }
-
-      try {
-        const user = await auth.me();
-        setUser(user);
-      } catch (err) {
-        localStorage.removeItem("token");
-        setUser(null);
-      } finally {
-        setLoading(false);
-      }
-
-      loadUser()
-    };
+    loadUser();
   }, []);
-}
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        logout,
+        loadUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
