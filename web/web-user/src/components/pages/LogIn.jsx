6389c91dbe18f9e8b5fa4da87d9899462f2cdf7a
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-07-20 19:24:28 +0200

    added LogIn component and JWT creation petition

--- web/web-user/src/components/pages/LogIn.jsx
+++ web/web-user/src/components/pages/LogIn.jsx
@@ -1,7 +1,56 @@
+// Packages
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+// Style
+import style from '../../style/pages/LogIn.module.css';
+
+// Files
+import Button from '../../../../shared/utils/Button.jsx';
+import EmailInput from '../../../../shared/utils/EmailInput.jsx';
+import PasswordInput from '../../../../shared/utils/PasswordInput.jsx';
+import { useAuth } from "../../../../shared/context/authContext.jsx";
+
 const LogIn = () => {
-    return(
-        <>
-        </>
-    )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogIn = async (e) => {
+    e.preventDefault();
+    console.log("web submit")
+
+    try {
+        const response = await login({email, password});
+        console.log(response);
+        navigate("/");
+    
+    } catch (err) {
+        console.error(err || "Something went wrong... {catch}")
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogIn} className={style.LogIn}>
+      <EmailInput
+        label="E-mail address"
+        placeholder="E-mail address"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <PasswordInput
+        label="Password"
+        placeholder="Password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button type="submit" value="Log In" />
+    </form>
+  );
+};
 export default LogIn;
